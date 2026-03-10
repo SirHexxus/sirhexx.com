ba32e5182664d75795ea2b2413dc030fef15397e
James Stacy <jamesmichaelstacy@gmail.com>
2026-03-10 00:24:37 -0700

    feat: add Umami analytics tracking

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,6 +9,7 @@ const Document = () => (
         href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@600;700&family=Space+Grotesk:wght@300;400;500&family=JetBrains+Mono:wght@400;500&family=VT323&display=swap"
         rel="stylesheet"
       />
+      <script defer src="https://analytics.sirhexx.com/script.js" data-website-id="d0d54e90-ff62-4a3e-8230-d7704f75c960" />
     </Head>
     <body>
       <Main />
